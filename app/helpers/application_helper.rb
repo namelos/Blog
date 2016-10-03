21066cea7bbc58eff66ffc59d0922f9fab3f8d08
Fred Wang <namelos@outlook.com>
2016-10-03 15:07:31 +0800

    add org mode parser

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
+require 'org-ruby'
+
 module ApplicationHelper
   def logged_in?
     not request.authorization.nil?
+  end
+
+  def org(text)
+    html = Orgmode::Parser.new(text).to_html
+    html.html_safe
   end
 end
