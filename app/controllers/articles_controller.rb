259077378281f76160459dd37f210c84b10219b3
Fred Wang <namelos@outlook.com>
2016-10-03 11:46:52 +0800

    add basic auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: USER_ID, password: PASSWORD, except: [:index, :show]
+
   def index
     @articles = Article.all
   end
